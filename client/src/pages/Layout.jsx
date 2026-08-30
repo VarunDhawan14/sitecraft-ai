a47ae32ec023bc5f8b1cb5bf73894867d8fcd3e0
Varun Dhawan <vddhawan9@gmail.com>
2026-08-30 23:59:33 +0530

    feat: implement route layouts with authentication checks and protected navigation

---
+++ client/src/pages/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
+import Loading from "../components/Loading";
+
+export function AuthLayout() {
+  const { user, loadingUser } = useAppContext();
+  if (loadingUser) return <Loading />;
+  if (!user) return <Navigate to='/login' replace />;
+  return <Outlet />;
+}
+
+export function GuestLayout() {
+  // if (loadingUser) return <Loading />;
+  // if (user) return <Navigate to='/' replace />;
+  return <Outlet />;
+}
