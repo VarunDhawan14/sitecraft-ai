7f79639f4910fc8e71870f36474f00c19305b251
Varun Dhawan <vddhawan9@gmail.com>
2026-09-02 19:45:08 +0530

    feat: implement reusable MongoDB connection handler

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export async function connectToDatabase() {
+  mongoose.connection.on("connected", () => {
+    console.log("Successfully Connected to MongoDB");
+  });
+  await mongoose.connect(process.env.MONGODB_URI);
+}
+
